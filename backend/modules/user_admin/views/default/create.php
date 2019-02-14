264f3e49e376bbb7f0363ac38066254cf76a954b
Kiwi <Kiwi@Kiwi_pc>
2019-02-14 10:45:57 +0800

    修改新建用户的新建

--- backend/modules/user_admin/views/default/create.php
+++ backend/modules/user_admin/views/default/create.php
@@ -1,43 +1,13 @@
 <?php
-
 use yii\helpers\Html;
-use yii\web\View;
 
-/* @var $this View */
-$this->title = Yii::t('app', '{New}{User}', [
-    'New' => Yii::t('app', 'New'), 'User' => Yii::t('app', 'User')
-]);
-
+/* @var $this yii\web\View */
+$this->title = "新增用户";
 ?>
-
 <h1>
     <?php echo $this->title ?>
 </h1> 
-
 <div class="create-user">
-    
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title" id="myModalLabel"><?= Html::encode($this->title) ?></h4>
-            </div>
-            
-            <div class="modal-body">
-                <?= $this->render('_form', ['model'=>$model] ) ?>
-            </div>    
-                
-            <div class="modal-footer">
-                <?= Html::button(Yii::t('app', 'Confirm'), [
-                    'class'=>'btn btn-primary btn-flat',  'data-dismiss' => 'modal', 'aria-label' => 'Close'
-                ]) ?>
-            </div>
-                
-       </div>
-    </div>
-
+    <?= $this->render('_form', ['model'=>$model] ) ?>
 </div>
 

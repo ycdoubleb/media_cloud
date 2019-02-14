73e350bb8982f43e6b4a26a98681c7db7c1ae69f
Vito915 <lwjun0915@qq.com>
2019-02-14 15:49:33 +0800

    前台-修改媒体库标签信息的获取方式

--- frontend/modules/media_library/searchs/MediaSearch.php
+++ frontend/modules/media_library/searchs/MediaSearch.php
@@ -4,9 +4,7 @@ namespace frontend\modules\media_library\searchs;
 
 use common\models\media\Media;
 use common\models\media\MediaAttValueRef;
-use common\models\media\MediaTagRef;
 use common\models\media\MediaType;
-use common\models\Tags;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use yii\helpers\ArrayHelper;
@@ -49,87 +47,95 @@ class MediaSearch extends Media
     }
     
     /**
-     * Creates data provider instance with search query applied
-     *
+     * 查询媒体总数
      * @param array $params
-     *
-     * @return ActiveDataProvider
+     * @return array
      */
     public function search($params)
     {
-        $page = ArrayHelper::getValue($params, 'page', 1);      //分页
-        $limit = ArrayHelper::getValue($params, 'limit', 20);   //显示数
-        
-        $query = self::find()->select(['Media.id'])->from(['Media' => Media::tableName()]);
         $this->load($params);
+        $att_value_ids = array_filter($this->attribute_value_id ? $this->attribute_value_id : []);  //需要查找的属性
+
+        $query = self::find()->select(['Media.id','Media.name'])->from(['Media' => Media::tableName()]);
         
         // 主要过滤条件
         $query->andFilterWhere([
             'Media.del_status' => 0,
-            'Media.status' => Media::STATUS_PUBLISHED,  // 发布状态
+//            'Media.status' => Media::STATUS_PUBLISHED,  // 发布状态
             'Media.type_id' => $this->type_id
-        ]);
-        // 属性值条件
-        if(!empty($this->attribute_value_id)){
-            foreach ($this->attribute_value_id as $value) {
-                $query->andFilterWhere (['AttrValueRef.attribute_value_id' => $value]);
-            }
-        }
+        ]);        
+        // 属性值过滤条件
+        if (count($att_value_ids) > 0) {
+            // 关联媒体属性值关系表
+            $query->leftJoin(['AttrValueRef' => MediaAttValueRef::tableName()], '(AttrValueRef.media_id = Media.id AND AttrValueRef.is_del = 0)');
+            $query->andFilterWhere (['AttrValueRef.attribute_value_id' => $att_value_ids]);
+        }        
         // 模糊查询
         $query->andFilterWhere(['or',
             ['like', 'Media.name', $this->keyword],
-            ['like', 'Tags.name', $this->keyword],
+            ['like', 'Media.tags', $this->keyword],
         ]);
-
-        // 关联媒体标签关系表
-        $query->leftJoin(['TagRef' => MediaTagRef::tableName()], '(TagRef.object_id = Media.id and TagRef.is_del = 0)');
-        // 关联标签表
-        $query->leftJoin(['Tags' => Tags::tableName()], 'Tags.id = TagRef.tag_id');
-        // 关联查询媒体类型
-        $query->leftJoin(['MediaType' => MediaType::tableName()], 'MediaType.id = Media.type_id');
-        // 关联媒体属性值关系表
-        $query->leftJoin(['AttrValueRef' => MediaAttValueRef::tableName()], '(AttrValueRef.media_id = Media.id and AttrValueRef.is_del = 0)');
+        
         //以媒体id为分组
         $query->groupBy(['Media.id']);
         //查询总数
-        $totalCount = $query->count('id');
-        
-        // 查询媒体数据
-        $query->addSelect(['Media.id','cover_url', 'Media.name', 'dir_id', 'MediaType.name AS type_name', 
-                'MediaType.sign AS type_sign', 'price', 'duration', 'size', 
-        ]);
-        // 显示数量
-        $query->offset(($page - 1) * $limit)->limit($limit);
-        // 查询媒体结果
-        $meidaResult = $query->asArray()->all();
-        
-        // 获取显示的媒体ID
-        $mediaIds = [];
-        foreach ($meidaResult as $value) {
-            $mediaIds[] .= $value['id'];
-        }
-        // 查询媒体下的标签
-        $tagRefQuery = MediaTagRef::getTagsByObjectId($mediaIds, false);
-        $tagRefQuery->addSelect(["GROUP_CONCAT(Tags.`name` ORDER BY TagRef.id ASC SEPARATOR ',') AS tag_name"]);
-        // 查询标签结果
-        $tagRefResult = $tagRefQuery->asArray()->all(); 
-        
-        // 以media_id为索引
-        $medias = ArrayHelper::index($meidaResult, 'id');
-        $results = ArrayHelper::index($tagRefResult, 'object_id');
-
-        // 合并查询后的结果
-        foreach ($medias as $id => $item) {
-            if(isset($results[$id])){
-                $medias[$id] += $results[$id];
-            }
-        }
+        $totalCount = $query->count();
         
         return [
             'filter' => $params,
             'total' => $totalCount,
+        ];
+    }
+    
+    /**
+     * 查询媒体数据（ajax请求时调用）
+     * @param array $params
+     * @return ActiveDataProvider
+     */
+    public function searchMediaData($params)
+    {
+        $this->load($params);
+        $page = ArrayHelper::getValue($params, 'page', 1);      //分页
+        $limit = ArrayHelper::getValue($params, 'limit', 20);   //显示数
+        $att_value_ids = array_filter($this->attribute_value_id ? $this->attribute_value_id : []);  //需要查找的属性
+        
+        // 查询媒体数据
+        $query = self::find()->select(['Media.id', 'Media.id','cover_url', 'Media.name', 'dir_id', 'MediaType.name AS type_name', 
+                'MediaType.sign AS type_sign', 'price', 'duration', 'size', 'Media.tags AS tag_name'
+            ])->from(['Media' => Media::tableName()]);
+        
+        // 主要过滤条件
+        $query->andFilterWhere([
+            'Media.del_status' => 0,
+//            'Media.status' => Media::STATUS_PUBLISHED,  // 发布状态
+            'Media.type_id' => $this->type_id
+        ]);        
+        // 属性值过滤条件
+        if (count($att_value_ids) > 0) {
+            // 关联媒体属性值关系表
+            $query->leftJoin(['AttrValueRef' => MediaAttValueRef::tableName()], '(AttrValueRef.media_id = Media.id AND AttrValueRef.is_del = 0)');
+            $query->andFilterWhere (['AttrValueRef.attribute_value_id' => $att_value_ids]);
+        }
+        // 模糊查询
+        $query->andFilterWhere(['or',
+            ['like', 'Media.name', $this->keyword],
+            ['like', 'Media.tags', $this->keyword],
+        ]);
+        
+        // 关联查询媒体类型
+        $query->leftJoin(['MediaType' => MediaType::tableName()], 'MediaType.id = Media.type_id');
+        //以媒体id为分组
+        $query->groupBy(['Media.id']);
+        
+        // 显示数量
+        $query->offset(($page - 1) * $limit)->limit($limit);
+        // 查询媒体结果
+        $mediaResult = $query->asArray()->all();
+       
+        return [
+            'filter' => $params,
             'data' => [
-                'media' => $medias
+                'media' => $mediaResult
             ],
         ];
     }

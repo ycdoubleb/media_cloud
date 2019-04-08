1e7a5110c9e634057c8927237900b2043d821715
Kiwi <Kiwi@Kiwi_pc>
2019-04-08 09:31:17 +0800

    1、添加单击行选中

--- backend/modules/media_admin/views/media/____media_table_dom.php
+++ backend/modules/media_admin/views/media/____media_table_dom.php
@@ -1,7 +1,6 @@
 <?php
 
 use common\models\media\Media;
-use common\utils\DateUtil;
 use common\widgets\grid\GridViewChangeSelfColumn;
 use common\widgets\tagsinput\TagsInputAsset;
 use yii\grid\GridView;
@@ -15,7 +14,7 @@ TagsInputAsset::register($this);
 
 ?>
 
-<?= GridView::widget([
+<?=GridView::widget([
     'id' => 'grid-view',
     'dataProvider' => $dataProvider,
     'layout' => "{items}\n{summary}\n{pager}",  
@@ -315,6 +314,16 @@ TagsInputAsset::register($this);
 $js = <<<JS
     
     $("input[data-role=tagsinput]").tagsinput();
+        
+    $('#grid-view table tbody tr').each(function(){
+        $(this).click(function(){
+            if($(this).children(":first").children('input[name="selection[]"]').is(':checked')){
+                $(this).children(":first").children('input[name="selection[]"]').prop("checked",false);
+            }else{
+                $(this).children(":first").children('input[name="selection[]"]').prop("checked",true);
+            }
+        });
+    });
         
 JS;
     $this->registerJs($js,  View::POS_READY);
